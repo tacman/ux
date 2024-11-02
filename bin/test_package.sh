8397b52530dbe8e4f031d36ce6a9cd0b6d111442
Hugo Alliaume <hugo@alliau.me>
2024-11-02 17:22:11 +0100

    [DX] Rework testing process, add `test` script to packages package.json

--- bin/test_package.sh
+++ bin/test_package.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# This script is used to test an UX package.
+# It also handle the case where a package has multiple versions of a peerDependency defined.
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PROJECT_DIR=$(dirname "$SCRIPT_DIR")
+
 # Flag to track if any test fails
 all_tests_passed=true
+
+# Check if we have at least one argument
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied, please provide the package's path."
+fi
 
 # Check if jq is installed
 if ! command -v jq &> /dev/null; then
@@ -11,16 +23,31 @@ fi
 
 runTestSuite() {
     echo -e "Running tests for $workspace...\n"
-    yarn workspace $workspace run -T vitest --run || { all_tests_passed=false; }
+    yarn run -T vitest --run || { all_tests_passed=false; }
 }
 
 processWorkspace() {
-    local workspace="$1"
-    local location="$2"
+    local location="$1"
 
-    echo -e "Processing workspace $workspace at location $location...\n"
+    if [ ! -d "$location" ]; then
+        echo "No directory found at $location"
+        return
+    fi
 
     package_json_path="$location/package.json"
+    if [ ! -f "$package_json_path" ]; then
+        echo "No package.json found at $package_json_path"
+        return
+    fi
+    
+    workspace=$(jq -r '.name' "$package_json_path")
+    if [ -z "$workspace" ]; then
+        echo "No name found in package.json at $package_json_path"
+        return
+    fi
+    
+    echo -e "Processing workspace $workspace at location $location...\n"
+
     echo "Checking '$package_json_path' for peerDependencies with multiple versions defined"
     deps_with_multiple_versions=$(jq -r '.peerDependencies | to_entries[] | select(.value | contains("||")) | .key' "$package_json_path")
 
@@ -48,21 +75,7 @@ processWorkspace() {
     fi
 }
 
-# Iterate over each workspace using process substitution
-while IFS= read -r workspace_info; do
-    # Split the workspace_info into workspace and location
-    workspace=$(echo "$workspace_info" | jq -r '.name')
-    location=$(echo "$workspace_info" | jq -r '.location')
-    
-    # Skip the root workspace
-    if [ $workspace == "null" ]; then
-        continue
-    fi
-
-    # Call the function to process the workspace
-    processWorkspace "$workspace" "$location"
-
-done < <(yarn workspaces list --json)
+processWorkspace "$(realpath "$PWD/$1")"
 
 # Check the flag at the end and exit with code 1 if any test failed
 if [ "$all_tests_passed" = false ]; then
